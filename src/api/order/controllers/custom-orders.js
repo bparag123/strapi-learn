8eb13ff481dd57dc57d7237aa930a20983a4690a
parag.b <parag.b@simformsolutions.com>
2023-11-01 18:43:28 +0530

    Entity Service Implementation

--- src/api/order/controllers/custom-orders.js
+++ src/api/order/controllers/custom-orders.js
@@ -5,7 +5,30 @@ const { createCoreController } = require("@strapi/strapi").factories;
 module.exports = createCoreController("api::order.order", ({ strapi }) => ({
   async customOrders(ctx) {
     const orders = await strapi.db.query("api::order.order").findMany();
-    //custom logic here
-    return orders;
+
+    const entry = await strapi.entityService.findMany("api::order.order", {
+      
+      filters: {
+        status: 'pending'
+      },
+
+      populate: {
+        customer: {
+          fields: ['name', 'email'],
+        },
+      },
+
+      // sort: ['id:desc'],
+      sort: {
+        customer: {
+          createdAt: "asc",
+        },
+      },
+
+      //Pagination Options
+      start: 0,
+      limit: 1,
+    });
+    return entry;
   },
 }));
